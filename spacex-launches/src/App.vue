0c572bc1c6abf12f707630e5292eb348aee9c8db
Desteur Fall <shmurdanoah@gmail.come>
2025-05-24 15:19:46 +0200

    install dependencies

--- spacex-launches/src/App.vue
+++ spacex-launches/src/App.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <div class="text-3xl font-bold text-center">Tailwind fonctionne !</div>
+</template>
 
 <style scoped></style>
 <script setup lang="ts"></script>
